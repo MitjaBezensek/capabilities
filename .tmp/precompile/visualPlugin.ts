7e839ab72b27bcdbe6c2a9d09e189a6d68c45b14
Mitja Bezenšek <mitja.bezensek@gmail.com>
2018-01-24 13:58:22 +0100

    Initial commit.

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var capabilitiesF9EF0176A2E640F0BA5D9FCF8921870A = {
+        name: 'capabilitiesF9EF0176A2E640F0BA5D9FCF8921870A',
+        displayName: 'capabilities',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.9.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.capabilitiesF9EF0176A2E640F0BA5D9FCF8921870A.Visual(options),
+        custom: true
+    };
+}
